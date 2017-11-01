2eaed785757b73e71a48694c315f01f04ff74721
Baptiste POLVE <baptiste.polve@telecom-sudparis.eu>
2017-11-01 14:53:51 +0100

    update to integrate apiddoc

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express();
 
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.redirect('/api/permissions');
+    res.redirect('/apidoc/')
 });
 
 
